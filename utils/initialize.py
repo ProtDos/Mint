14d3de3aa053fee7eaa14cb4a6ff13b7fee816dd
Bernward Sanchez <109844019+pneb@users.noreply.github.com>
2022-11-01 15:52:56 +0800

    Update initialize.py

--- utils/initialize.py
+++ utils/initialize.py
@@ -3,7 +3,7 @@ from utils.utils import Utils
 
 class Initialize:
     def __init__(self, username):
-        self.social_media_list = ["github", "roblox", "facebook", "linkedin", "replit", "paypal", "youtube", "twitch", "tiktok", "instagram", "pornhub", "onlyfans", "twitter", "steam", "reddit", "feds", "discord"]
+        self.social_media_list = ["github", "roblox", "facebook", "linkedin", "replit", "paypal", "youtube", "twitch", "tiktok", "instagram", "pornhub", "onlyfans", "twitter", "steam", "reddit", "feds", "discord", "ogu"]
         self.username = username
         self.keyword_list = ["404", "500", "Found", "Not found", "ip", "IP", "ipaddress", "IPADDRESS", "success", "Success"]
         self.username_checker = UsernameChecker(username, self.social_media_list)
